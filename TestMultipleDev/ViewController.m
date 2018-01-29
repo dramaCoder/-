5076126850dc912f93872917dd0b36d82bc4ec43
Darren Xu <darrentennis@163.com>
2018-01-30 03:27:50 +0800

    没东西

--- TestMultipleDev/ViewController.m
+++ TestMultipleDev/ViewController.m
@@ -20,11 +20,6 @@
     NSLog(@"继续合作");
 }
 
-- (void)fixbug{
-    NSLog(@"在分支修复bug");
-}
-
-
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
