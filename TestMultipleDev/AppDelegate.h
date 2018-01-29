ec39695ff9e23ff34695622530256109b87390b6
Darren Xu <darrentennis@163.com>
2018-01-30 02:11:51 +0800

    Initial Commit

---
+++ TestMultipleDev/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestMultipleDev
+//
+//  Created by Darren  xu on 2018/1/30.
+//  Copyright © 2018年 Darren  xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestMultipleDev/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestMultipleDev
+//
+//  Created by Darren  xu on 2018/1/30.
+//  Copyright © 2018年 Darren  xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
