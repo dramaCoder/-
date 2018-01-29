c760e5cc9d48aa0749d0e5b0cd266b92e469ecdb
Darren Xu <darrentennis@163.com>
2018-01-30 03:06:13 +0800

    添加person类

---
+++ TestMultipleDev/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  TestMultipleDev
+//
+//  Created by Darren  xu on 2018/1/30.
+//  Copyright © 2018年 Darren  xu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ TestMultipleDev/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  TestMultipleDev
+//
+//  Created by Darren  xu on 2018/1/30.
+//  Copyright © 2018年 Darren  xu. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
