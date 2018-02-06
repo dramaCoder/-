1d50edc899d7952eae0cf0d58ecf2a06696c7e0b
Darren Xu <darrentennis@163.com>
2018-02-07 03:41:17 +0800

    sdjsdj

--- TestMultipleDev/ViewController.m
+++ TestMultipleDev/ViewController.m
@@ -18,29 +18,23 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    NSLog(@"开始协同开发");
-    NSLog(@"继续合作");
-    _arr = [NSMutableArray new];
-    NSLog(@"测试冲突-张三做的修改");
-    NSLog(@"测试冲突-Alice做的修改");
 }
 
-- (void)fixbug{
-    NSLog(@"切换到分支，修复以前的bug");
+- (void)testConflict{
+    NSLog(@"测试合并产生的冲突");
 }
 
 - (void)newFunction{
     NSLog(@"切换到分支，开发新功能");
 }
 
-- (void)testConflict{
-    NSLog(@"测试合并冲突");
-}
 
 - (void)testBranch{
     NSLog(@"在分支上的修改，直接提交到master");
     NSLog(@"测试结果：并不能直接提交到版本库的master，只能提交到版本库的该分支上，没有该分支则会自动创建分支");
 }
+
+// 以下是版本备份  在分支修复bug  合并分支
 
 - (void)phaseOnefinish{
     NSLog(@"1.0版本的开发完毕，保存备份");
