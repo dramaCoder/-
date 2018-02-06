b7ef846c1858e59ed4fc34b29f8a65a35260c8c2
Darren Xu <darrentennis@163.com>
2018-02-07 01:04:25 +0800

    删除某些

--- TestMultipleDev/ViewController.m
+++ TestMultipleDev/ViewController.m
@@ -38,9 +38,6 @@
     NSLog(@"测试结果：并不能直接提交到版本库的master，只能提交到版本库的该分支上，没有该分支则会自动创建分支");
 }
 
-- (void)nextModel{
-    NSLog(@"开始2.0版本的开发");
-}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
