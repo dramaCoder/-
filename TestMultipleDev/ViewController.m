8bb75d16572cfcbb169b68b3eeda273e086cf271
Darren Xu <darrentennis@163.com>
2018-02-07 03:29:10 +0800

    随便提交

--- TestMultipleDev/ViewController.m
+++ TestMultipleDev/ViewController.m
@@ -33,6 +33,10 @@
     NSLog(@"切换到分支，开发新功能");
 }
 
+- (void)testConflict{
+    NSLog(@"测试合并冲突");
+}
+
 - (void)testBranch{
     NSLog(@"在分支上的修改，直接提交到master");
     NSLog(@"测试结果：并不能直接提交到版本库的master，只能提交到版本库的该分支上，没有该分支则会自动创建分支");
