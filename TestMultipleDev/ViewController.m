fab6c1d43adb975905a603e7ddbd2a1c049683ca
Darren Xu <darrentennis@163.com>
2018-02-07 04:17:36 +0800

    解决合并冲突

--- TestMultipleDev/ViewController.m
+++ TestMultipleDev/ViewController.m
@@ -20,8 +20,12 @@
     [super viewDidLoad];
 }
 
+- (void)testConflictServer{
+    NSLog(@"---------服务器端的修改");
+}
 - (void)testConflictLocal{
     NSLog(@"本地的修改");
+    NSLog(@"选择保留两者");
 }
 
 - (void)newFunction{
